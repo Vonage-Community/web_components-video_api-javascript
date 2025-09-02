77c1c616e3116b58611e3d3986fd13bea732b46e
Dwane Hemmings <dwane.hemmings@vonage.com>
2025-09-02 03:13:38 +0000

    reworked some parts and README for live-poll-control

--- live-poll-control/src/LivePollControl.js
+++ live-poll-control/src/LivePollControl.js
@@ -177,28 +177,28 @@ export class LivePollControl extends LitElement {
     return html`
       <div id="container" part="container">
         <h1 id="title-preview" part="title-preview">${this.pollTitleText}</h1>
-        <input .value="${live(this.pollTitleText)}" @input=${(e) => this.pollTitleText = e.target.value} ?disabled=${this.pollStarted} placeholder="${this.pollTitlePlaceholder}" id="poll-title" name="poll-title-input" part="title-input"></input>
+        <input .value="${live(this.pollTitleText)}" @input=${(e) => this.pollTitleText = e.target.value} ?disabled=${this.pollStarted} placeholder="${this.pollTitlePlaceholder}" id="poll-title" name="poll-title-input" part="input title"></input>
         <ul id="options" part="options">
           ${this.options.map(
             (option, index) => html`
                 <li part="option">
-                  <input .value="${option.text}" ?disabled=${this.pollStarted} @input=${(e) => this.__updateOption(e, index)} id="option-text-${index}" name="option-text-${index}" part="option-input"></input><button @click=${this.__removeOption} ?disabled=${this.pollStarted} data-index="${index}" data-option="${option}" part="remove-button">${this.removeButtonText}</button>
+                  <input .value="${option.text}" ?disabled=${this.pollStarted} @input=${(e) => this.__updateOption(e, index)} id="option-text-${index}" name="option-text-${index}" part="option-input"></input><button @click=${this.__removeOption} ?disabled=${this.pollStarted} data-index="${index}" data-option="${option}" part="button remove">${this.removeButtonText}</button>
                   <progress id="option-progress-${index}" name="option-progress-${index}" value="${this.totalVotes === 0 ? 0 : (option.votes / this.totalVotes) * 100}" max="100" part="option-progress">${this.totalVotes === 0 ? 0 : (option.votes / this.totalVotes) * 100} %</progress><output name="option-result-${index}" for="option-progress-${index}" part="option-output">${option.votes}</output>
                 </li>
               `
           )}
         </ul>
         <form @submit=${this.__addOption} id="form" part="form">
-          <input type="text" name="optionTxt" placeholder="${this.placeholder}" id="optionTxt" part="input" ?disabled=${this.pollStarted}></input>
-          <button type="submit" ?disabled=${this.pollStarted}>${this.inputButtonText}</button>
+          <input type="text" name="optionTxt" placeholder="${this.placeholder}" id="optionTxt" part="input option" ?disabled=${this.pollStarted}></input>
+          <button type="submit" ?disabled=${this.pollStarted} part="button option">${this.inputButtonText}</button>
         </form>
         ${
           this.pollStarted
-            ? html`<button @click=${this.__stopPoll} ?disabled=${!this.pollStarted} part="stop-button">${this.stopButtonText}</button>`
-            : html`<button @click=${this.__startPoll} ?disabled=${this.pollStarted} part="start-button">${this.startButtonText}</button>`          
+            ? html`<button @click=${this.__stopPoll} ?disabled=${!this.pollStarted} part="button stop">${this.stopButtonText}</button>`
+            : html`<button @click=${this.__startPoll} ?disabled=${this.pollStarted} part="button start">${this.startButtonText}</button>`          
         }
-        <button @click=${this.__resetPoll} part="reset-button">${this.resetButtonText}</button>
-        <button @click=${this.__closePoll} part="close-button">${this.closeButtonText}</button>
+        <button @click=${this.__resetPoll} part="button reset">${this.resetButtonText}</button>
+        <button @click=${this.__closePoll} part="button close">${this.closeButtonText}</button>
       <div>
     `;
   }
